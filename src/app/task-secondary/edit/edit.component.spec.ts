98d9ee0cbb4f30c7313e1e97584ce1482dd90e2d
BolteAdrian <bolte_adrian@yahoo.com>
2022-07-18 21:53:35 +0300

    Clean code

--- src/app/task-secondary/edit/edit.component.spec.ts
+++ src/app/task-secondary/edit/edit.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EditComponent } from './edit.component';
 
 describe('EditComponent', () => {
@@ -8,9 +7,8 @@ describe('EditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
-    })
-    .compileComponents();
+      declarations: [EditComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EditComponent);
     component = fixture.componentInstance;
